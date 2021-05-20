669938b8ec82c7310d3dde96399551f044051c23
objektwerks <objektwerks@runbox.com>
2021-05-20 08:25:08 -0400

    refactored router test server shutdown

--- src/test/scala/medical/RouterTest.scala
+++ src/test/scala/medical/RouterTest.scala
@@ -11,13 +11,15 @@ import com.typesafe.config.ConfigFactory
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.concurrent.Await
 
-class RouterTest extends AnyWordSpec with Matchers with ScalatestRouteTest  {
+class RouterTest extends AnyWordSpec with BeforeAndAfterAll with Matchers with ScalatestRouteTest  {
   val conf = ConfigFactory.load("router.conf")
   val sparkInstanceConf = conf.as[SparkInstanceConf]("spark")
 
@@ -34,8 +36,20 @@ class RouterTest extends AnyWordSpec with Matchers with ScalatestRouteTest  {
   val server = Http()
     .newServerAt(host, port)
     .bindFlow(router.api)
+    .map { server =>
+      logger.info(s"*** Server started at: ${server.localAddress.toString}")
+      server
+    }
 
-  logger.info(s"*** Test Server started at: http://$host:$port")
+  override protected def afterAll(): Unit =
+    server
+      .flatMap(_.unbind())
+      .onComplete { _ =>
+        logger.info("*** Server shutting down...")
+        actorRefFactory.terminate()
+        Await.result(actorRefFactory.whenTerminated, 3.seconds)
+        logger.info("*** Server shutdown.")
+      } 
 
   import de.heikoseeberger.akkahttpupickle.{UpickleSupport => Upickle}
   import Upickle._
